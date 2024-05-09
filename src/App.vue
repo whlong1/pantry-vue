dad7344a9835c121445c33f29f17b9621806343d
Hunter Long <whunterlong@gmail.com>
2024-05-09 13:53:21 -0400

    Add base styling

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,15 @@
 </script>
 
 <template>
-  <nav>
-    Navbar
-  </nav>
-  <main>
-    Main content
-  </main>
+  <div class="flex flex-col h-full">
+    <nav class="flex-none w-full p-4 text-sm font-medium items-center justify-between border-b border-gray-700">
+      Navbar
+    </nav>
+    <main>
+      <h1>Main content</h1>
+    </main>
+    <footer class="flex-none p-4 border-t border-gray-700">
+      Footer
+    </footer>
+  </div>
 </template>
-
-<style scoped>
-
-</style>
