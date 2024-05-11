2fe8896e2d475ff247582aaa66895b4f313869be
Hunter Long <whunterlong@gmail.com>
2024-05-10 22:38:28 -0400

    Add food service

---
+++ src/services/foodService.js
@@ -0,0 +1,23 @@
+const BASE_URL = `${import.meta.env.VITE_SERVERLESS_URL}/food`;
+import * as tokenService from "./tokenService";
+
+const createFood = async (imageUrl) => {
+  try {
+    const res = await fetch(`${BASE_URL}`, {
+      method: "POST",
+      headers: {
+        "Authorization": `Bearer ${tokenService.getToken()}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ imageUrl }),
+    });
+    return await res.json();
+  } catch (error) {
+    console.log("Error:", error);
+  }
+};
+
+
+export { 
+  createFood,
+};
