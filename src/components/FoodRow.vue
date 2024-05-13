aa834e5fb7c3952c7918d5556e67a4aabce77bc1
whlong1 <whunterlong@gmail.com>
2024-05-12 20:21:38 -0400

    Fix units

--- src/components/FoodRow.vue
+++ src/components/FoodRow.vue
@@ -1,7 +1,7 @@
 <template>
   <tr class="border-gray-200 border-b text-xs font-regular">
     <td class="border-b overflow-hidden py-2 font-medium w-48">{{ truncatedFoodName }}</td>
-    <td class="border-b overflow-hidden py-2 text-left">{{ food.estimatedCaloriesPerServing }} grams</td>
+    <td class="border-b overflow-hidden py-2 text-left">{{ food.estimatedCaloriesPerServing }} calories</td>
     <td class="border-b overflow-hidden py-2 text-left">{{ food.estimatedFatGramsPerServing }} grams</td>
     <td class="border-b overflow-hidden py-2 text-left">{{ food.estimatedCarbGramsPerServing }} grams</td>
     <td class="border-b overflow-hidden py-2 text-left">{{ food.estimatedProteinGramsPerServing }} grams</td>
